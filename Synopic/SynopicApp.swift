617458630398583f3f59721caa8a1602e7439f89
Adrian Lemus <16787847+LEMUSADR000@users.noreply.github.com>
2022-12-15 16:29:12 -0600

    Initial Commit

---
+++ Synopic/SynopicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SynopicApp.swift
+//  Synopic
+//
+//  Created by Adrian Lemus on 12/15/22.
+//
+
+import SwiftUI
+
+@main
+struct SynopicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
