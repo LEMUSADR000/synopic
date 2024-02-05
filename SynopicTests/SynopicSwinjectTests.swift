0c354a562aff477a91dcc6d266f7e67d7a103f1a
Bundys <16787847+LEMUSADR000@users.noreply.github.com>
2024-02-04 20:49:25 -0800

    synopic swinject tests

--- SynopicTests/SynopicSwinjectTests.swift
+++ SynopicTests/SynopicSwinjectTests.swift
@@ -14,52 +14,52 @@ final class SynopicSwinjectTests: XCTestCase {
 
   func testCoordinatorFactories() {
     let appRoot = appAssembler.resolve(AppRootCoordinator.self)
-    XCTAssert(appRoot != nil, "AppRootCoordinator is nil")
+    XCTAssert(appRoot != nil, "AppRootCoordinator failed to initialize")
     
     let noteRootCoordinator = appAssembler.resolve(NoteCreateCoordinator.self)
-    XCTAssert(noteRootCoordinator != nil, "NoteCreateCoordinator is nil")
+    XCTAssert(noteRootCoordinator != nil, "NoteCreateCoordinator failed to initialize")
     
     let noteGroupDetailsCoordinator = appAssembler.resolve(
       NoteGroupDetailsCoordinator.self,
       argument: Group(title: "Title", author: "Author", childCount: 12)
     )
-    XCTAssert(noteGroupDetailsCoordinator != nil, "NoteGroupDetailsCoordinator is nil")
+    XCTAssert(noteGroupDetailsCoordinator != nil, "NoteGroupDetailsCoordinator failed to initialize")
   }
   
   func testServiceFactories() {
     let ocrService = appAssembler.resolve(OCRService.self)
-    XCTAssert(ocrService != nil, "OCRService is nil")
+    XCTAssert(ocrService != nil, "OCRService failed to initialize")
     
     let persistentStore = appAssembler.resolve(PersistentStore.self)
-    XCTAssert(persistentStore != nil, "PersistentStore is nil")
+    XCTAssert(persistentStore != nil, "PersistentStore failed to initialize")
     
     let summariesRepository = appAssembler.resolve(SummariesRepository.self)
-    XCTAssert(summariesRepository != nil, "SummariesRepository is nil")
+    XCTAssert(summariesRepository != nil, "SummariesRepository failed to initialize")
     
     let chatGptService = appAssembler.resolve(ChatGPTService.self)
-    XCTAssert(chatGptService != nil, "ChatGPTService is nil")
+    XCTAssert(chatGptService != nil, "ChatGPTService failed to initialize")
     
     let cameraManager = appAssembler.resolve(CameraManager.self)
-    XCTAssert(cameraManager != nil, "CameraManager is nil")
+    XCTAssert(cameraManager != nil, "CameraManager failed to initialize")
     
     let cameraService = appAssembler.resolve(CameraService.self)
-    XCTAssert(cameraService != nil, "CameraService is nil")
+    XCTAssert(cameraService != nil, "CameraService failed to initialize")
   }
   
   func testViewModelFactories() {
     let landingViewModel = appAssembler.resolve(LandingViewModel.self)
-    XCTAssert(landingViewModel != nil, "LandingViewModel is nil")
+    XCTAssert(landingViewModel != nil, "LandingViewModel failed to initialize")
     
     let noteCreateViewModel = appAssembler.resolve(NoteCreateViewModel.self)
-    XCTAssert(noteCreateViewModel != nil, "NoteCreateViewModel is nil")
+    XCTAssert(noteCreateViewModel != nil, "NoteCreateViewModel failed to initialize")
     
     let notesGridViewModel = appAssembler.resolve(
       NotesGridViewModel.self,
       argument: Group(title: "title", author: "author")
     )
-    XCTAssert(notesGridViewModel != nil, "NotesGridViewModel is nil")
+    XCTAssert(notesGridViewModel != nil, "NotesGridViewModel failed to initialize")
     
     let cameraViewModel = appAssembler.resolve(CameraViewModel.self)
-    XCTAssert(cameraViewModel != nil, "CameraViewModel is nil")
+    XCTAssert(cameraViewModel != nil, "CameraViewModel failed to initialize")
   }
 }
